19854a420e9895965374250d4665c9bc1765409f
Anthony <99anthonylam@gmail.com>
2020-12-13 00:38:18 -0800

    add basic crud endpoints

---
+++ server/routes/index.js
@@ -0,0 +1,82 @@
+var express = require('express');
+var router = express.Router();
+var request = require('request');
+const {MongoClient} = require('mongodb');
+const {Translate} = require('@google-cloud/translate').v2;
+require('dotenv').config();
+
+//Setup Google Translate Client
+const CREDENTIALS = JSON.parse(process.env.CREDENTIALS);
+
+const translate = new Translate({
+    credentials: CREDENTIALS,
+    projectId: CREDENTIALS.project_id
+});
+
+//Helpers
+async function listLanguages() {
+  const [languages] = await translate.getLanguages();
+  return languages
+}
+
+async function translateText(text, targetLanguage){
+    let [response] = await translate.translate(text, targetLanguage);
+    return response;
+}
+
+MongoClient.connect(process.env.MONGO_CONNECTION, { useUnifiedTopology: true })
+.then(client => {
+  var db = client.db('user-tweets').collection("tweets")
+
+  //Read DB
+  router.get('/getTweets/:userId', (req, res) => {
+    db.find().toArray().then(results => {
+      res.send(results)
+    }) .catch (error => console.log(error))
+  })
+
+  //Post to DB & Twitter
+  router.post('/writeTweet/:txt', (req, res) => {
+    //Tweet code
+    db.insertOne(newItem).then(results => {
+      console.log(results)
+      res.send(200)
+    }) .catch (error => console.log(error))
+  })
+  
+  //Fetch languages and codes supported by Google Translate
+  router.get('/getLanguages', (req, res) => {
+    listLanguages()
+    .then((response)=> {
+      res.send(response);
+    })
+    .catch((err) => {
+      res.send(err);
+    })
+  })
+  
+  //Translate text to target lang
+  router.get('/translate/:txt/:lang', (req, res) => {
+    translateText(req.params.txt, req.params.lang)
+    .then((response)=> {
+      res.send(JSON.parse(response));
+    })
+    .catch((err) => {
+      res.send(err);
+    })
+  })
+  
+  //Fetch gif from GIPHY
+  router.get('/getGif/:search_term', (req, res) => {
+    request('https://api.giphy.com/v1/gifs/search?api_key=' + process.env.GIPHY 
+    + '&limit=1&q=' + req.params.search_term +'&lang=en', function (error, response, body) {
+      if (!error && response.statusCode == 200) {
+        res.send(JSON.parse(body).data[0].embed_url)
+      }
+    })
+  })
+  
+})
+.catch(error => console.error(error))
+
+module.exports = router;
