ea09d3bd04a9f1a8ca650bb5e4ba07982bc55043
Anthony <99anthonylam@gmail.com>
2020-12-14 12:58:11 -0800

    add fetch tweet endpoint

--- app/client/src/utils/apiRoutes.js
+++ app/client/src/utils/apiRoutes.js
@@ -35,13 +35,22 @@ export const getGif = (msg) => {
     }));
   };
 
-  //Helper for postTweet
-  function getCookie(name) {
+   //Helper for cookies
+   function getCookie(name) {
     const value = `; ${document.cookie}`;
     const parts = value.split(`; ${name}=`);
     if (parts.length === 2) return parts.pop().split(';').shift();
   }
 
+  export const getTweets = (id) => {
+    const getTweetsFail = "GET_TWEETS_FAIL";
+    return axios.get(BASE_URL+"/getTweets" + "/" + getCookie("reqToken") + "/"
+    + getCookie("reqSecret") + "/" + getCookie("oauth_verifier")).catch((error) => ({
+      type: getTweetsFail,
+      error,
+    }));
+  };
+ 
   export const postTweet = (text, gifUrl) => {
     const postTweetFail = "POST_TWEET_FAIL";
     return axios.post(BASE_URL+"/writeTweet" + "/" + getCookie("reqToken") + "/"
